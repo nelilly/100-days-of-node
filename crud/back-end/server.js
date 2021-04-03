3b6cdeec64fee7621b6da7da948e92a451c44100
Paul Dill <paul@pdill.dev>
2021-04-02 22:15:32 -1000

    CRUD create (POST) function implementation with MariaDB.  No DB being uploaded here

--- crud/back-end/server.js
+++ crud/back-end/server.js
@@ -1,7 +1,6 @@
 const mariadb = require('mariadb')
 const express = require('express')
 const cors = require('cors')
-const logger = require('morgan')
 require('dotenv').config()
 const app = express()
 const router = express.Router()
@@ -19,88 +18,48 @@ const pool = mariadb.createPool({
 
 })
 
-let selectResponse
-
-const dbCall = () => {
-  pool.getConnection(err => {
-    if (err) {
-      console.log(err)
-      return
-    }
-    console.log('Connection successful, you faka.')
-  })
-    .then(conn => {
-      conn.query('SELECT * FROM authors')
-        .then(res => { selectResponse = res })
-        .catch(err => console.log(err))
-    })
+const dbGetCall = () => {
+  pool.query('SELECT * FROM authors')
+    .then(res => { return res })
+    .catch(err => console.log(err))
+  pool.end()
+    .then(() => console.log('Connection ended'))
     .catch(err => console.log(err))
 }
 
-router.use(logger('dev'))
-router.use(express.json())
-router.use(express.urlencoded({ extended: false }))
+const dbPostCall = (last, first, country) => {
+  pool.getConnection()
+    .then(conn => conn.query('INSERT INTO authors(name_last, name_first, country) VALUES(?, ?, ?);', [last, first, country])
+      .then(res => {
+        console.log(res)
+        conn.release()
+      })
+      .catch(err => console.log(err))
+    )
+  // pool.end()
+  //   .then(() => console.log('Connection ended'))
+  //   .catch(err => console.log(err))
+}
 
-router.use(
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(
   cors({
     origin: 'http://localhost:3000',
     credentials: true
-  }), function (req, res, next) {
-    //   res.set('Content-Type', 'application/json')
-    //   res.header('Access-Control-Allow-Origin', '*')
-    console.log('Something is happening.')
-    next()
-  })
+  }))
 
-router.get('/',
-  (req, res) => {
-    dbCall()
-    res.send(selectResponse)
+app.route('/')
+  .get((req, res) => {
+    dbGetCall()
+    res.send()
+  })
+  .post((req, res) => {
+    console.log(JSON.parse(Object.keys(req.body)).last)
+    dbPostCall(JSON.parse(Object.keys(req.body)).last, JSON.parse(Object.keys(req.body)).first, JSON.parse(Object.keys(req.body)).country)
+    res.send('Entry accepted!')
   })
 
 module.exports = router
-//     let sql = 'SELECT * FROM authors;'
-//     try{
-//         let conn
-//         // const rows = await conn.query("INSERT INTO authors(name_last, name_first, country) VALUES(?,?,?)",
-//         // [])
-//         const res = await conn.query(sql)
-//         // console.log(res)
-//     }
-//     catch(err){
-//         console.log(err)
-//     }
-//     finally{
-//         if(conn) conn.release()
-//     }
-// }
-
-// app.get('/', (req, res) => {
-//     console.log(dbCall())
-// })
-
-// mariadb.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: 'books_library',
-//     timezone: 'Pacific/Honolulu',
-//     multipleStatements: true,
-//     supportBigInt: true
-
-// })
-// .then(conn => {
-//     conn.query("SELECT * FROM authors")
-//     .then(rows => {
-// console.log(rows)
-// return conn.query("INSERT INTO authors(name_last, name_first, country) VALUES(?)",
-// ["Godin", "Seth", "USA"])
-// })
-//     .then(console.log('Connection Successful, you faka.'))
-//     .then(res => console.log(res))
-//     .catch(err=> console.log(err))
-
-// })
-//     .catch(err=> console.log(err))
 
 app.listen(PORT, () => `Connection successful on port ${PORT}.`)
--- crud/front-end/books_library/src/App.js
+++ crud/front-end/books_library/src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import Axios from 'axios'
-import router from '../src/server'
+// import router from '../src/server'
 
 function App () {
   const [isLoading, setIsLoading] = useState(false)
+  const [lastName, setLastName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [countryName, setCountryName] = useState('')
 
   useEffect(() => {
     const apiReturned = false
     if (isLoading === true) {
-      Axios.get('/api', router)
+      Axios.get('http://localhost:4000/')
         .then(res => console.log(res))
         .catch(err => console.log(err))
         .finally(() => { return !apiReturned })
@@ -18,6 +21,23 @@ function App () {
       return !isLoading
     }
   }, [isLoading])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(lastName, firstName, countryName)
+    Axios.post('http://localhost:4000/', {
+      last: `${lastName}`,
+      first: `${firstName}`,
+      country: `${countryName}`
+    },
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
 
   return (
     <div className="App">
@@ -28,17 +48,22 @@ function App () {
     <div className="get">
     <button onClick={() => setIsLoading(true)} >Click dat faka!</button>
     </div>
-    {/* <div>
-      {isLoading ? }
-    </div> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <form onSubmit={handleSubmit}>
+      <h2>Add author to library</h2>
+        <label>
+        <p>Last name</p>
+        <textarea type='text' onChange={event => setLastName(event.target.value)} value={lastName}/>
+        </label>
+        <label>
+        <p>First name</p>
+        <textarea type='text' onChange={event => setFirstName(event.target.value)} value={firstName}/>
+        </label>
+        <label>
+        <p>Country of origin</p>
+        <textarea type='text' onChange={event => setCountryName(event.target.value)} value={countryName}/>
+        </label>
+        <button type='submit'>Submit</button>
+    </form>
       </header>
     </div>
   )
